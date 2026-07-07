0f699441820a872c52ad039716c3dff7a996e4a5
chduce259-collab <chduce259@gmail.com>
2026-07-07 18:10:44 +0330

    Update game.js

--- game.js
+++ game.js
@@ -1,65 +1,133 @@
 let military = 61;
 let popularity = 65;
 
+let currentEvent = 0;
+
+const events = [
+
+{
+title:"ارتش عراق از مرز عبور کرد",
+text:"نیروهای عراقی وارد خاک ایران شده‌اند.",
+
+choices:[
+{
+name:"اعلام بسیج عمومی",
+success:"بسیج موفق بود.",
+military:5,
+popularity:3
+},
+
+{
+name:"تمرکز بر دفاع از خوزستان",
+success:"دفاع از خوزستان تقویت شد.",
+military:3,
+popularity:1
+}
+]
+},
+
+{
+title:"خرمشهر در خطر است",
+text:"نیروهای عراقی به خرمشهر نزدیک شده‌اند.",
+
+choices:[
+{
+name:"اعزام نیروهای بیشتر",
+success:"نیروهای کمکی رسیدند.",
+military:4,
+popularity:2
+},
+
+{
+name:"تخلیه غیرنظامیان",
+success:"بخش بزرگی از مردم نجات یافتند.",
+military:0,
+popularity:5
+}
+]
+}
+
+];
+
 function rollDice(){
 
 return Math.floor(Math.random()*20)+1;
 
 }
 
-function choice1(){
+function makeChoice(choiceIndex){
 
 let dice = rollDice();
 
-if(dice >= 15){
+let choice =
+events[currentEvent].choices[choiceIndex];
 
-military += 5;
-popularity += 3;
+if(dice >= 10){
 
-alert("بسیج موفق بود.");
+alert(choice.success);
 
-}
+military += choice.military;
 
-else{
+popularity += choice.popularity;
 
-military += 2;
+}else{
 
-alert("بسیج با موفقیت نسبی انجام شد.");
+alert("عملیات با شکست نسبی روبرو شد.");
+
+military -= 2;
+
+popularity -= 1;
 
 }
 
 updateStats();
 
+currentEvent++;
+
+if(currentEvent < events.length){
+
+loadEvent();
+
+}else{
+
+document.getElementById("event").innerHTML =
+"<h2>پایان نسخه آزمایشی</h2>";
+
+document.getElementById("choices").innerHTML =
+"";
 }
-
-function choice2(){
-
-let dice = rollDice();
-
-if(dice >= 15){
-
-popularity += 5;
-
-alert("دفاع موفقیت‌آمیز بود.");
-
-}
-
-else{
-
-popularity -= 2;
-
-alert("دفاع با مشکلاتی روبرو شد.");
-
-}
-
-updateStats();
 
 }
 
 function updateStats(){
 
-document.getElementById("military").innerText = military;
+document.getElementById("military").innerText =
+military;
 
-document.getElementById("popularity").innerText = popularity;
+document.getElementById("popularity").innerText =
+popularity;
 
 }
+
+function loadEvent(){
+
+document.getElementById("event").innerHTML =
+
+`<h3>${events[currentEvent].title}</h3>
+<p>${events[currentEvent].text}</p>`;
+
+document.getElementById("choices").innerHTML =
+
+`
+<button onclick="makeChoice(0)">
+${events[currentEvent].choices[0].name}
+</button>
+
+<button onclick="makeChoice(1)">
+${events[currentEvent].choices[1].name}
+</button>
+`;
+
+}
+
+window.onload = loadEvent;
